013295f5737be4f1aa165db71e2bd587fff9e778
Josh Adell <josh.adell@gmail.com>
2013-10-09 00:43:03 -0400

    Added shopping cart and layout

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ $products = array(
 $app = new \Slim\Slim(array(
 	'view' => new \Slim\Views\Twig(),
 ));
+$app->add(new \Slim\Middleware\SessionCookie());
 
 // Default route
 $app->get('/', function () use ($app, $products) {
@@ -36,6 +37,33 @@ $app->get('/', function () use ($app, $products) {
 $app->get('/product/:code', function ($code) use ($app, $products) {
 	$product = $products[$code];
 	$app->render('product.html', array('product' => $product, 'code' => $code));
+});
+
+// Shopping cart route
+$app->get('/cart', function () use ($app, $products) {
+	$cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+	$app->render('cart.html', array('cart' => $cart, 'products' => $products));
+});
+
+// Add to cart
+$app->post('/add-to-cart', function () use ($app) {
+	$code = $app->request->post('code');
+
+	if (!isset($_SESSION['cart'])) {
+		$_SESSION['cart'] = array();
+	}
+	if (!isset($_SESSION['cart'][$code])) {
+		$_SESSION['cart'][$code] = 0;
+	}
+	$_SESSION['cart'][$code]++;
+
+	$app->redirect('/cart');
+});
+
+// Empty cart
+$app->post('/clear-cart', function () use ($app) {
+	$_SESSION['cart'] = array();
+	$app->redirect('/cart');
 });
 
 $app->run();
